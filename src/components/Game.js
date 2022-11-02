a199c422951b31bb883013a986b38fa70f6c5f2b
Erick Gonzalez <62159920+erickcgt@users.noreply.github.com>
2022-11-02 15:49:54 -0400

    added draft game to game page

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,24 @@
+function submit(){
+    var gamepage = document.getElementById("gamepagediv");
+    console.log(gamepage);
+    var display = document.createElement("p")
+    display.textContent = display.textContent = document.getElementById("search").value;
+    gamepage.appendChild(display);
+    return;
+}
+
 function GamePage(){
 
    
 
     return(
-        <h1>Game Page!</h1>
+        <div id="gamepagediv">
+            <h1>Game Page!</h1>
+            <div className="search-container">
+                <input type="text" placeholder="Search...." id="search"></input>
+                <button onClick={submit}>Submit</button>
+            </div>
+        </div>
     );
 }
 
