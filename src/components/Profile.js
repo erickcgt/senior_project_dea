ba1634116aecf31b95656c2c8d6c1894b85605ee
Erick Gonzalez <erickgonzalezecgt@gmail.com>
2022-11-27 13:26:19 -0500

    updated profilepage to show actual user data dynamically instead of placeholder data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,9 +12,33 @@ import {
  
 } from 'mdb-react-ui-kit';
 
-export default function ProfilePage() {
-
-    const container = {
+export default class ProfilePage extends React.Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        userInfo: null
+      };
+    }
+    componentDidMount(){
+      fetch("http://localhost:5000/userInfo", 
+        {
+          method: "POST",
+          crossDomain:true,
+          headers:{
+            "Content-Type":"application/json",
+            Accept:"application/json",
+            "Access-Control-Allow-Origin":"*",
+        },
+        body:JSON.stringify({
+          token:window.localStorage.getItem("token"),
+        }),
+        }).then((res)=>res.json())
+        .then(data=>{
+          this.setState({userInfo: data.data});
+        });
+    }
+    render(){
+      const container = {
         display: "block",        
         marginLeft: "auto",
         marginRight: "auto",        
@@ -22,34 +46,27 @@ export default function ProfilePage() {
         paddingTop: "50px"
     
       };
-
       const heading = {
         fontFamily: "Gluten",
         color: "#2613fe",
         fontSize: "40px",
         paddingBottom: "10px",
         textDecorationLine: "underline"
-    
       };
-
       const userCard = {                
         outline: "5px solid #2613fe"      
       }
-
       const progressCard = {    
         outline: "5px solid #f07552"           
       }
-
       const line = {
         color: "#2613fe",
         opacity: "100"
       }
-
       const progressText = {
         color: "black",
         paddingBottom: "20px"
       }
-
       const progressHeader = {
         color: "black",
         textDecorationLine: "underline",
@@ -57,34 +74,25 @@ export default function ProfilePage() {
         paddingBottom: "21px",
         paddingTop: "20px"
       }    
-
       const spaceAfterBar = {
         paddingBottom: "35px"
       }
-
       const cardSpacing = {
         paddingLeft: "20px"
       }
       
-    //fetch user data
-    var userInfo = {};
-    fetch("http://localhost:5000/userInfo", {
-        method: "POST",
-        crossDomain:true,
-        headers:{
-            "Content-Type":"application/json",
-            Accept:"application/json",
-            "Access-Control-Allow-Origin":"*",
-        },
-        body:JSON.stringify({
-            token:window.localStorage.getItem("token"),
-        }),
-        }).then((res)=>res.json())
-        .then(data=>{
-            userInfo = data;
-        });
-    console.log("USERINFO: ",userInfo)
-    return (
+      if(this.state.userInfo == null){
+        return <div></div>
+      }
+      var fullName = this.state.userInfo["fname"] + " " + this.state.userInfo["lname"];
+      var email = this.state.userInfo["email"];
+      var gameScore = this.state.userInfo["gamescore"].reduce((a, b) => a + b, 0);
+      var gameMax = this.state.userInfo["gamescore"].length;
+      var gamePercentage = Math.floor(gameScore/gameMax * 100);
+      var learnScore = this.state.userInfo["learnscore"].reduce((a, b) => a + b, 0);
+      var learnMax = this.state.userInfo["learnscore"].length;
+      var learnPercentage = Math.floor(learnScore/learnMax * 100);
+      return (
         <section style={container}>
             <h4 style={heading}>My Profile</h4>
         <MDBContainer className="py-5">         
@@ -103,7 +111,7 @@ export default function ProfilePage() {
                         <MDBCardText>Full Name</MDBCardText>
                     </MDBCol>
                     <MDBCol sm="9">
-                        <MDBCardText>John Doe</MDBCardText>
+                        <MDBCardText>{fullName}</MDBCardText>
                     </MDBCol>
                     </MDBRow>
                     <hr style={line}/>
@@ -112,7 +120,7 @@ export default function ProfilePage() {
                         <MDBCardText>Email</MDBCardText>
                     </MDBCol>
                     <MDBCol sm="9">
-                        <MDBCardText>example@example.com</MDBCardText>
+                        <MDBCardText>{email}</MDBCardText>
                     </MDBCol>
                     </MDBRow>
                     <hr style={line}/>
@@ -132,14 +140,14 @@ export default function ProfilePage() {
                 <MDBCard className="mb-4" style={progressCard}>
                 <MDBCardBody>
                 <MDBCardText className="mb-4" style={progressHeader}>My Progress</MDBCardText>
-                        <MDBCardText className="mb-1" style={progressText}>Learn - (5/6 Completed)</MDBCardText>
+                        <MDBCardText className="mb-1" style={progressText}>Learn - ({learnScore}/{learnMax} Completed)</MDBCardText>
                         <MDBProgress className="rounded" height='30'>
-                        <MDBProgressBar striped animated width={83} valuemin={0} valuemax={100}> 83% </MDBProgressBar>
+                        <MDBProgressBar striped animated width={learnPercentage} valuemin={0} valuemax={100}> {learnPercentage}% </MDBProgressBar>
                         </MDBProgress>
                         <div style={spaceAfterBar}></div>
-                        <MDBCardText className="mt-4 mb-1" style={progressText}>Game - (3/5 Completed)</MDBCardText>
+                        <MDBCardText className="mt-4 mb-1" style={progressText}>Game - ({gameScore}/{gameMax} Completed)</MDBCardText>
                         <MDBProgress className="rounded" height='30'>
-                        <MDBProgressBar striped animated width={60} valuemin={0} valuemax={100}> 60% </MDBProgressBar>
+                        <MDBProgressBar striped animated width={gamePercentage} valuemin={0} valuemax={100}> {gamePercentage}% </MDBProgressBar>
                         </MDBProgress>
                         <div style={spaceAfterBar}></div>
                     
@@ -151,5 +159,7 @@ export default function ProfilePage() {
             </MDBRow>
         </MDBContainer>
         </section>
-  );
+      );
+    }
+    
 }
